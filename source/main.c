85d85377e21e072189b76c56d12e3c730e0a3e17
Jonas Segundo <jsegu010@wch136-24.cs.ucr.edu>
2021-10-10 17:21:29 -0700

    working switch pt 2

--- source/main.c
+++ source/main.c
@@ -11,71 +11,89 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-enum SM1_STATES{SM1_START, SM1_RELEASEONB0, SM1_RELEASEONB1, SM1_PRESSONB0,SM1_PRESSONB1} State;
+enum SM2_STATES{SM2_START,SM2_INCREASE,SM2_DECREASE, SM2_WAIT,SM2_RESET} State;
 
 
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0x00;
-	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x07;
     /* Insert your solution below */
-	State = SM1_START;
-    while (1) {
+	State = SM2_START;
+    while (1){  
 	switch(State){
-        
-	case SM1_START:
-        State = SM1_RELEASEONB0;
-        break;
-        
-	case SM1_RELEASEONB0:
-        if((PINA&0x01) == 0x01){ State = SM1_PRESSONB1;} 
-	else if((PINA&0x01) == 0x00){ State = SM1_RELEASEONB0;}
+	case: SM2_START:
+	PORTC = 0x07;
+	State = SM2_WAIT;
 	break;
 	
-	case SM1_PRESSONB1:
-	if((PINA&0x01) == 0x01){ State = SM1_PRESSONB1;}
-        else if((PINA&0x01) == 0x00){ State = SM1_RELEASEONB1;}
+	case: SM2_WAIT;
+	if((PINA & 0x01) == 0x01 && (PINA & 0x02) = 0x02){
+		State = SM2_RESET;
+	}  
+	else if((PINA & 0x01) == 0x01){
+		State = SM2_INCREASE;
+	}
+	else if((PINA & 0x02) = 0x02){
+		State = SM2_DECREASE;
+	}
+	else{
+	State = SM2_WAIT;
+	}
 	break;
-
-	case SM1_RELEASEONB1:
-	if((PINA&0x01) == 0x01){ State = SM1_PRESSONB0;}
-        else if((PINA&0x01) == 0x00){ State = SM1_RELEASEONB1;}
-        break;
 	
-	case SM1_PRESSONB0:
-        if((PINA&0x01) == 0x01){ State = SM1_PRESSONB0;}
-        else if((PINA&0x01) == 0x00){ State = SM1_RELEASEONB0;}
-        break;
-
+	case SM2_INCREASE:
+	 if((PINA & 0x01) == 0x01 && (PINA & 0x02) = 0x02){
+                State = SM2_RESET;
+        }
+        else if((PINA & 0x01) == 0x01){
+                State = SM2_INCREASE;
+        }
+        else if((PINA & 0x02) = 0x02){
+                State = SM2_DECREASE;
+        }
+        else{
+        State = SM2_WAIT;
+        }
+	break;
+	
+	case SM2_DECREASE:
+	 if((PINA & 0x01) == 0x01 && (PINA & 0x02) = 0x02){
+                State = SM2_RESET;
+        }
+        else if((PINA & 0x01) == 0x01){
+                State = SM2_INCREASE;
+        }
+        else if((PINA & 0x02) = 0x02){
+                State = SM2_DECREASE;
+        }
+        else{
+        State = SM2_WAIT;
+        }
+	break;
+	
+	case SM2_RESET:
+	 if((PINA & 0x01) == 0x01 && (PINA & 0x02) = 0x02){
+                State = SM2_RESET;
+        }
+        else if((PINA & 0x01) == 0x01){
+                State = SM2_INCREASE;
+        }
+        else if((PINA & 0x02) = 0x02){
+                State = SM2_DECREASE;
+        }
+        else{
+        State = SM2_WAIT;
+        }
+	break;
+	
 	default:
-	State = SM1_START;
-	break;
+	State = SM2_START;
 	}
-	
+
 	switch(State){
 	
-	case SM1_START:
-        PORTB =0x01;
-        break;
-
-        case SM1_RELEASEONB0:
-        PORTB = 0x01;
-        break;
-
-        case SM1_PRESSONB1:
-        PORTB = 0x02;
-        break;
-
-        case SM1_RELEASEONB1:
-        PORTB = 0x02;
-        break;
-
-        case SM1_PRESSONB0:
-        PORTB = 0x01;
-        break;
-	}
-
-        
+	}		     
     }
     return 1;
 }
